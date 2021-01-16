d02872ac29607b358d261db626d59900fabbb118
Julius Freiny <julius.freiny@gmx.de>
2021-01-16 20:46:09 +0100

    tests added

--- DFA/DFA.Logic.ConsoleTestApp/Test.cs
+++ DFA/DFA.Logic.ConsoleTestApp/Test.cs
@@ -1,33 +1,34 @@
-﻿using DFA.Objects;
+﻿using DFA.Logic;
+using DFA.Objects;
 using DFA.Objects.Interfaces;
 using System;
 
-namespace DFA.Logic.Tests
+namespace DFA.ConsoleTestApp
 {
     public class Test
     {
         private static void Main(string[] args)
         {
             char[] alphabet = new char[] { '0', '1' };
-            State a = new State("A", StateType.Init);
-            State b = new State("B", StateType.Other);
-            State c = new State("C", StateType.Final);
-            State d = new State("D", StateType.Final);
-            State e = new State("E", StateType.Other);
-            State f = new State("F", StateType.Final);
+            IState a = new State("A", StateType.Init);
+            IState b = new State("B", StateType.Other);
+            IState c = new State("C", StateType.Final);
+            IState d = new State("D", StateType.Final);
+            IState e = new State("E", StateType.Other);
+            IState f = new State("F", StateType.Final);
 
-            Transition a0 = new Transition(b, '0');
-            Transition a1 = new Transition(e, '1');
-            Transition b0 = new Transition(b, '0');
-            Transition b1 = new Transition(c, '1');
-            Transition c0 = new Transition(d, '0');
-            Transition c1 = new Transition(c, '1');
-            Transition d0 = new Transition(d, '0');
-            Transition d1 = new Transition(f, '1');
-            Transition e0 = new Transition(e, '0');
-            Transition e1 = new Transition(e, '1');
-            Transition f0 = new Transition(e, '0');
-            Transition f1 = new Transition(f, '1');
+            ITransition a0 = new Transition(b, '0');
+            ITransition a1 = new Transition(e, '1');
+            ITransition b0 = new Transition(b, '0');
+            ITransition b1 = new Transition(c, '1');
+            ITransition c0 = new Transition(d, '0');
+            ITransition c1 = new Transition(c, '1');
+            ITransition d0 = new Transition(d, '0');
+            ITransition d1 = new Transition(f, '1');
+            ITransition e0 = new Transition(e, '0');
+            ITransition e1 = new Transition(e, '1');
+            ITransition f0 = new Transition(e, '0');
+            ITransition f1 = new Transition(f, '1');
 
             a.AddTransition(a0);
             a.AddTransition(a1);
@@ -42,10 +43,10 @@ namespace DFA.Logic.Tests
             f.AddTransition(f0);
             f.AddTransition(f1);
 
-            IState[] states = new State[] { a, b, c, d, e, f };
-            ITransition[] transitions = new Transition[] { a0, a1, b0, b1, c0, c1, d0, d1, e0, e1, f0, f1 };
+            IState[] states = new IState[] { a, b, c, d, e, f };
+            ITransition[] transitions = new ITransition[] { a0, a1, b0, b1, c0, c1, d0, d1, e0, e1, f0, f1 };
 
-            string word = "0011";
+            string word = "0011111000";
 
             Controller controller = new Controller(states, alphabet, transitions, true);
             bool accepted = controller.Run(word.ToCharArray());
